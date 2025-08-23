3c866fdd5dc9ff4d949658ee648d8195a888c207
rafi tanujaya <rafibussines@gmail.com>
2025-08-23 15:13:58 +0700

    adding project

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
+import { AnimatedSpan, Terminal, TypingAnimation } from "@/components/magicui/terminal";
 
 export default function Home() {
   return (
     <>
-      <div>
-        Hallo
-      </div>
+    <div className="flex w-full min-h-screen items-center justify-center">
+      <Terminal>
+          <TypingAnimation>&gt; npm install coffeecodecommunity</TypingAnimation>
+        
+          <AnimatedSpan className="text-green-500">✔ UI/UX checks.</AnimatedSpan>
+        
+          <AnimatedSpan className="text-green-500">
+            ✔ Development Checks.
+          </AnimatedSpan>
+        
+          <TypingAnimation className="text-muted-foreground">
+            Web Sedang Dalam Tahap Pembuatan
+          </TypingAnimation>
+        </Terminal>
+    </div>
+
     </>
   );
 }
